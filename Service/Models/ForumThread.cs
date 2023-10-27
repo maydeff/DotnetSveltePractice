01ca19c4a0af00526ee58593218256fe5df6e584
maydeff <barteqdev@gmail.com>
2023-10-27 20:02:47 +0200

    removed unused column attribute

--- Service/Models/ForumThread.cs
+++ Service/Models/ForumThread.cs
@@ -1,11 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Service.Models
+﻿namespace Service.Models
 {
     public class ForumThread
     {
         public int Id { get; set; }
-        [Column("my_document_key")]
         public required string DocumentKey { get; set; }
         public required string Title { get; set; }
         public required string Author { get; set; }
